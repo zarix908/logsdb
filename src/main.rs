3a824202221f858ae4a8b18d8834742eca3cecbb
dkhairutdinov <d.khairutdinov@infinet.ru>
2022-07-08 19:35:29 +0500

    add query handling

--- src/main.rs
+++ src/main.rs
@@ -3,48 +3,46 @@ mod log;
 mod writer;
 
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder, error};
+use ::log::{error};
 use std::sync::mpsc::{sync_channel, SyncSender, TrySendError};
-use std::time::{SystemTime, UNIX_EPOCH};
-use std::{thread, time::Duration};
+use std::thread;
 
 use engine::Engine;
-use log::Log;
+use crate::log::Log;
 use writer::Writer;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let (sender, receiver) = sync_channel::<Log>(100);
-    let senderData = web::Data::new(sender);
 
-    HttpServer::new(move || App::new().app_data(senderData.clone()).service(insert))
+    let handle = thread::spawn(move || {
+        let writer = Writer::new().expect("create writer failed");
+        let mut engine = Engine::new(120, writer);
+        
+        for log in receiver {
+            let r = engine.insert(log);
+            if let Err(err) = r {
+                error!("insert log failed: {}", err)
+            }
+        }
+    });
+
+    let sender_data = web::Data::new(sender);
+    let r = HttpServer::new(move || App::new().app_data(sender_data.clone()).service(insert))
         .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+        .run().await;
+    if r.is_err() {
+        return r;
+    }
 
-    // let writer = Writer::new().expect("create writer failed");
-    // let mut engine = Engine::new(120, writer);
-
-    // for i in 0..4 {
-    //     let current_time = SystemTime::now()
-    //         .duration_since(UNIX_EPOCH)
-    //         .expect("Time went backwards")
-    //         .as_millis();
-    //     thread::sleep(Duration::from_millis(1000));
-    //     let log = Log {
-    //         timestamp: current_time,
-    //         ip: [192, 168, 103, 200 + i],
-    //         request: String::from("GET /api HTTP/1.1"),
-    //     };
-
-    //     engine.insert(log).expect("insert log failed");
-    // }
+    handle.join().or(Ok(()))
 }
 
 #[post("/insert")]
 async fn insert(log: web::Json<Log>, sender: web::Data<SyncSender<Log>>) -> Result<impl Responder, error::Error> {
-    let err = sender.try_send(log.0).err();
+    let err = sender.try_send(log.0);
 
-    if let Some(send_err) = err {
+    if let Result::Err(send_err) = err {
         return match send_err {
             TrySendError::Full(_) => Err(error::ErrorInternalServerError("insertion queue is full")),
             TrySendError::Disconnected(_) => Err(error::ErrorInternalServerError("internal error"))
